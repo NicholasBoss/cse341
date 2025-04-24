2f0bcbf94cda8f8a2c085c65185cc6e7a12a25a8
NicholasBoss <NicholasBoss0914@gmail.com>
2025-04-23 18:25:23 -0600

    separating out routes and created controller

---
+++ controllers/lesson1.js
@@ -0,0 +1,7 @@
+const peterRoute = (req, res) => {
+    res.send("Peter Ashworth");
+  };
+
+module.exports = {
+    peterRoute
+};
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-// express web server
 const express = require('express');
+const routes = require('./routes');
 const app = express();
 const port = 3000;
- 
-app.get('/', (req, res) => {
-  res.send("Peter Ashworth");
-});
+
+app.use('/', routes);
  
 app.listen(process.env.PORT || port, () => {
   console.log('Web Server is listening at http://localhost:' + (process.env.PORT || port));
